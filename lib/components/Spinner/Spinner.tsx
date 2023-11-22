544c2c2fb318ae61ef1fe61df91807ae36933209
Pedro Gardim <me@pedrogardim.com>
2023-11-22 20:31:02 +0100

    feat: add audio and click animation

--- lib/components/Spinner/Spinner.tsx
+++ lib/components/Spinner/Spinner.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import "./Spinner.css";
 interface SpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
@@ -18,13 +19,33 @@ const Spinner = ({
   speed = 1,
   ...props
 }: SpinnerProps) => {
+  const audio = useRef(new Audio("/assets/ayayay.mp3"));
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleSpinnerClick = () => {
+    audio.current.pause();
+    audio.current.currentTime = 0;
+    audio.current.play();
+    setIsClicked(true);
+    setTimeout(() => setIsClicked(false), 2000);
+  };
+
+  useEffect(() => {
+    audio.current.play();
+  }, []);
+
   return (
     <div
-      className="spinner-container"
+      className={`spinner-container ${isClicked && "spinner-clicked"}`}
       {...props}
       style={{ ...(props.style || {}), animationDuration: `${1 / speed}s` }}
+      onClick={handleSpinnerClick}
     >
-      <img src={`/assets/ochando_${variant}.png`} alt="ochando" />
+      <img
+        src={`/assets/ochando_${variant}.png`}
+        alt="ochando"
+        draggable="false"
+      />
     </div>
   );
 };
